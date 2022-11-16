84d80b9e628fb59219798c540e3ee87debd82744
Ian Oginski <Ian.Oginski@protonmail.com>
2022-11-16 10:57:41 +0100

    modified:   remote_status.sh -> added null arguments exception 	deleted:    remote_status_0.5.sh

--- remote_status.sh
+++ remote_status.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-
 HOSTNAME_CMD=$(hostname)
+DEBUGGING=off
 
 function script_usage() {
     cat << EOF
@@ -10,14 +9,73 @@ Usage:
      --help                  Displays this help
      --hostname                 Displays host name
      --ip                       Display IP addresses
+     --datestamp             Display datestamp
 EOF
 }
 
-function check_hostname() {
-    echo $(date +%Y%m%d_%X)
-    echo this vm: $HOSTNAME_CMD has following IP addresses: $(hostname -I|sed 's/\ /;/g')
+function script_exit() {
+    echo "invalid parameter was provided: $param"
+    script_usage
 }
 
-check_hostname;
+function check_hostname() {
+    echo this vm: $HOSTNAME_CMD
+}
+
+function check_ip() {
+    echo IP addresses: $(hostname -I|sed 's/\ /;/g')
+}
+function return_date(){
+    echo $(date +%Y%m%d_%X)
+    
+}
+
+#stub of a function
+function mydebug(){
+    [[ "$DEBUGGING" == "on" ]] && echo "do something" || echo "do nothing"
+    
+}
+
+
+
+function parse_params() {
+#catch if no arguments are provided
+[[ $# -eq 0 ]] && echo "no arguments provided"
+#else please iterate via arguments
+
+local param
+    while [[ $# -gt 0 ]]; do
+        param="$1"
+        shift
+        case $param in
+            --help)
+                script_usage
+                exit 0
+                ;;
+            --hostname)
+                check_hostname
+                ;;
+            --ip)
+                check_ip
+                ;;
+            --datestamp)
+                return_date
+                ;;
+            --mydebug)
+                mydebug
+                ;;
+            *)
+                script_exit
+                ;;
+        esac
+    done
+}
+
+#main function
+parse_params "$@"
+
+
+
+
 
 
